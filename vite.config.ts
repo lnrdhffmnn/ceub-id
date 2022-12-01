d93f49fcdd077960622116346b9100af9d755934
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-30 22:46:57 -0300

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "CEUB ID",
+        description: "CEUB ID",
+        lang: "pt-BR",
+        start_url: "/",
+        display: "standalone",
+        orientation: "portrait",
+        theme_color: "#f4f4f5",
+        background_color: "#f4f4f5",
+        icons: [
+          {
+            src: "/img/icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
