dd9fa0c2ac42c883dadb9e4e1905766a27b5dfd9
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-12-01 12:08:48 -0300

    Update home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,45 @@
-import { useState } from "react";
 import { QRCodeSVG } from "qrcode.react";
+import { useAtomValue } from "jotai";
+import { ceubIdAtom } from "../atoms/ceub-id";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const [ra, setRa] = useState<string>("12345678");
+  const [ceubId, setCeubId] = useState("");
+
+  const { ra, code } = useAtomValue(ceubIdAtom);
+
+  useEffect(() => {
+    setCeubId(`${ra}|${code}`);
+  }, [ra, code]);
 
   return (
     <>
-      <QRCodeSVG
-        className="shadow-xl rounded-xl"
-        value={ra}
-        size={280}
-        level="H"
-        includeMargin
-      />
-      <span className="absolute bottom-0 font-mono tracking-widest">{ra}</span>
+      {ra.length > 0 ? (
+        <>
+          <QRCodeSVG
+            className="shadow-xl rounded-xl"
+            value={ceubId}
+            size={280}
+            level="H"
+            includeMargin
+          />
+          <span className="absolute bottom-0 font-mono tracking-widest">
+            {ceubId}
+          </span>
+        </>
+      ) : (
+        <h2 className="text-3xl text-center w-3/4 mx-auto">
+          <span>Você precisa </span>
+          <Link
+            to="/settings"
+            className="text-fuchsia-600 hover:underline hover:decoration-dotted"
+          >
+            preencher
+          </Link>
+          <span> seus dados.</span>
+        </h2>
+      )}
     </>
   );
 }
