d299feeae989392d853e67d7bfad37ed8ffe4030
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-30 22:06:15 -0300

    Update app layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "./components/header";
 
 export default function App() {
   return (
-    <>
-      <header>
-        <h1>CEUB ID</h1>
-      </header>
-      <main>
+    <div className="w-screen h-screen flex flex-col items-center justify-center p-8 bg-zinc-100 font-sans">
+      <Header />
+      <main className="w-full h-full flex items-center justify-center flex-grow relative">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
