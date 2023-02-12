673eefdd423d6759466c99b3958cc8f5a6e1c53a
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-12 15:59:05 -0300

    Update home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,20 @@
+import { QRCodeSVG } from "qrcode.react";
+
 export default function Home() {
+  const ra = "00000000";
+
   return (
     <>
-      <p className="text-xl font-bold">Home</p>
+      <QRCodeSVG
+        value={ra}
+        size={250}
+        level="H"
+        includeMargin
+        className="rounded-xl shadow-xl border border-zinc-300"
+      />
+      <p className="font-mono font-bold tracking-widest absolute bottom-0">
+        {ra}
+      </p>
     </>
   );
 }
