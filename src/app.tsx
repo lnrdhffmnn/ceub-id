35d6458b7faf64ed14e2066b3c416ea21bdc9801
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-19 16:22:15 -0300

    Disable mobile zoom and text selection

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import Header from "./components/header";
 
 export default function App() {
   return (
-    <div className="w-screen h-screen flex flex-col items-stretch gap-8 p-8 bg-zinc-100 text-black font-sans">
+    <div className="w-screen h-screen flex flex-col items-stretch gap-8 p-8 bg-zinc-100 text-black font-sans select-none">
       <Header />
       <main className="w-full h-full flex flex-col items-center justify-center relative">
         <Outlet />
