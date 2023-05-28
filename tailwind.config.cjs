56dbe6be825300e3e52614aa233200272aadf5c6
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-05-28 20:07:12 -0300

    Update default font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
