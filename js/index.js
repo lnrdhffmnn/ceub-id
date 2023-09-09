3159fab1e6f5fdc6ca336d98b26f2f8c8f75f752
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-09-09 17:32:50 -0300

    Fix mobile issue

--- js/index.js
+++ js/index.js
@@ -1,147 +1,149 @@
 const Index = {
-  elements: {
-    /** @type {HTMLDialogElement} */
-    dialog: document.querySelector("dialog"),
+    elements: {
+        /** @type {HTMLDialogElement} */
+        dialog: document.querySelector("dialog"),
 
-    /** @type {NodeListOf<HTMLElement>} */
-    settingsBtn: document.querySelectorAll(".btn-dialog-open"),
+        /** @type {NodeListOf<HTMLElement>} */
+        settingsBtn: document.querySelectorAll(".btn-dialog-open"),
 
-    /** @type {NodeListOf<HTMLButtonElement>} */
-    btnDialogClose: document.querySelectorAll(".btn-dialog-close"),
+        /** @type {NodeListOf<HTMLButtonElement>} */
+        btnDialogClose: document.querySelectorAll(".btn-dialog-close"),
 
-    /** @type {HTMLFormElement} */
-    formSettings: document.querySelector("#form-settings"),
+        /** @type {HTMLFormElement} */
+        formSettings: document.querySelector("#form-settings"),
 
-    /** @type {HTMLInputElement} */
-    inputRa: document.querySelector('input[name="ra"]'),
+        /** @type {HTMLInputElement} */
+        inputRa: document.querySelector('input[name="ra"]'),
 
-    /** @type {HTMLParagraphElement} */
-    pRa: document.querySelector("#ra"),
+        /** @type {HTMLParagraphElement} */
+        pRa: document.querySelector("#ra"),
 
-    /** @type {HTMLDivElement} */
-    divQrcode: document.querySelector("#qrcode"),
-  },
+        /** @type {HTMLDivElement} */
+        divQrcode: document.querySelector("#qrcode"),
+    },
 
-  globals: {
-    localStorageKey: "ceub-id",
-    ra: "",
-  },
+    globals: {
+        localStorageKey: "ceub-id",
+        ra: "",
+    },
 
-  init() {
-    Index.addServiceWorker();
-    Index.loadRa();
-    Index.events();
-  },
+    init() {
+        Index.addServiceWorker();
+        Index.loadRa();
+        Index.events();
+    },
 
-  events() {
-    Index.elements.settingsBtn.forEach(btn => {
-      btn.addEventListener("click", () => {
-        Index.toggleDialog(true);
-      });
-    });
+    events() {
+        Index.elements.settingsBtn.forEach(btn => {
+            btn.addEventListener("click", () => {
+                Index.toggleDialog(true);
+            });
+        });
 
-    Index.elements.btnDialogClose.forEach(btn => {
-      btn.addEventListener("click", () => {
-        Index.toggleDialog(false);
-      });
-    });
+        Index.elements.btnDialogClose.forEach(btn => {
+            btn.addEventListener("click", () => {
+                Index.toggleDialog(false);
+            });
+        });
 
-    Index.elements.formSettings.addEventListener(
-      "submit",
-      Index.handleSettingsFormSubmit
-    );
-  },
+        Index.elements.formSettings.addEventListener(
+            "submit",
+            Index.handleSettingsFormSubmit
+        );
+    },
 
-  addServiceWorker() {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/service-worker.js", { scope: "/" });
-    }
-  },
+    addServiceWorker() {
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker.register("/service-worker.js", {
+                scope: "/",
+            });
+        }
+    },
 
-  /** @param {boolean} open */
-  toggleDialog(open) {
-    if (open) {
-      Index.loadRa();
-      Index.elements.dialog.showModal();
-    } else {
-      Index.elements.formSettings.reset();
-      Index.elements.dialog.close();
-    }
-  },
+    /** @param {boolean} open */
+    toggleDialog(open) {
+        if (open) {
+            Index.loadRa();
+            Index.elements.dialog.showModal();
+        } else {
+            Index.elements.formSettings.reset();
+            Index.elements.dialog.close();
+        }
+    },
 
-  /** @param {SubmitEvent} event */
-  handleSettingsFormSubmit(event) {
-    event.preventDefault();
+    /** @param {SubmitEvent} event */
+    handleSettingsFormSubmit(event) {
+        event.preventDefault();
 
-    /** @type {HTMLFormElement} */
-    const form = event.target;
-    const formData = new FormData(form);
+        /** @type {HTMLFormElement} */
+        const form = event.target;
+        const formData = new FormData(form);
 
-    const ra = formData.get("ra").toString();
+        const ra = formData.get("ra").toString();
 
-    try {
-      Index.saveRa(ra);
-      Index.toggleDialog(false);
-    } catch (/** @type {Error} */ error) {
-      alert(error.message);
-    }
-  },
+        try {
+            Index.saveRa(ra);
+            Index.toggleDialog(false);
+        } catch (/** @type {Error} */ error) {
+            alert(error.message);
+        }
+    },
 
-  loadRa() {
-    Index.globals.ra =
-      localStorage.getItem(Index.globals.localStorageKey) ?? "";
+    loadRa() {
+        Index.globals.ra =
+            localStorage.getItem(Index.globals.localStorageKey) ?? "";
 
-    Index.elements.inputRa.placeholder = Index.globals.ra;
-    Index.elements.inputRa.value = Index.globals.ra;
+        Index.elements.inputRa.placeholder = Index.globals.ra;
+        Index.elements.inputRa.value = Index.globals.ra;
 
-    Index.updateApp();
-  },
+        Index.updateApp();
+    },
 
-  /** @param {string} ra */
-  saveRa(ra) {
-    const raRegex = /^(\d{8})?$/;
+    /** @param {string} ra */
+    saveRa(ra) {
+        const raRegex = /^(\d{8})?$/;
 
-    if (!raRegex.test(ra)) {
-      throw new Error("Formato inválido");
-    }
+        if (!raRegex.test(ra)) {
+            throw new Error("Formato inválido");
+        }
 
-    localStorage.setItem(Index.globals.localStorageKey, ra);
-    Index.loadRa();
-    Index.updateApp();
-  },
+        localStorage.setItem(Index.globals.localStorageKey, ra);
+        Index.loadRa();
+        Index.updateApp();
+    },
 
-  updateApp() {
-    const { ra } = Index.globals;
-    const { divQrcode } = Index.elements;
+    updateApp() {
+        const { ra } = Index.globals;
+        const { divQrcode } = Index.elements;
 
-    Index.elements.pRa.innerHTML = ra;
+        Index.elements.pRa.innerHTML = ra;
 
-    if (ra.length) {
-      divQrcode.innerHTML = "";
-      const qr = new QRCode(divQrcode);
-      qr.makeCode(ra);
-      divQrcode.querySelector("img").draggable = false;
-    } else {
-      divQrcode.innerHTML = "";
+        if (ra.length) {
+            divQrcode.innerHTML = "";
+            const qr = new QRCode(divQrcode);
+            qr.makeCode(ra);
+            divQrcode.querySelector("img").draggable = false;
+        } else {
+            divQrcode.innerHTML = "";
 
-      const span1 = document.createElement("span");
-      span1.innerHTML = "Você precisa";
+            const span1 = document.createElement("span");
+            span1.innerHTML = "Você precisa";
 
-      const anchor = document.createElement("a");
-      anchor.innerHTML = "preencher";
-      anchor.classList.add("btn-dialog-open");
-      anchor.addEventListener("click", () => {
-        Index.toggleDialog(true);
-      });
+            const anchor = document.createElement("a");
+            anchor.innerHTML = "preencher";
+            anchor.classList.add("btn-dialog-open");
+            anchor.addEventListener("click", () => {
+                Index.toggleDialog(true);
+            });
 
-      const span2 = document.createElement("span");
-      span2.innerHTML = "seus dados.";
+            const span2 = document.createElement("span");
+            span2.innerHTML = "seus dados.";
 
-      divQrcode.appendChild(span1);
-      divQrcode.appendChild(anchor);
-      divQrcode.appendChild(span2);
-    }
-  },
+            divQrcode.appendChild(span1);
+            divQrcode.appendChild(anchor);
+            divQrcode.appendChild(span2);
+        }
+    },
 };
 
 Index.init();
--- service-worker.js
+++ service-worker.js
@@ -5,40 +5,40 @@ const sw = self;
 
 const CACHE_NAME = "ceub-id";
 const CACHE_URLS = [
-  "/",
-  "/index.html",
-  "/css/index.css",
-  "/js/index.js",
-  "/lib/qrcodejs/js/qrcode.js",
-  "/fonts/Inter.ttf",
-  "/fonts/JetBrainsMono.ttf",
-  "/img/icons/favicon.ico",
+    "/",
+    "/index.html",
+    "/css/index.css",
+    "/js/index.js",
+    "/lib/qrcodejs/js/qrcode.js",
+    "/fonts/Inter.ttf",
+    "/fonts/JetBrainsMono.ttf",
+    "/img/icons/favicon.ico",
 ];
 
 sw.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_URLS))
-  );
-  sw.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_URLS))
+    );
+    sw.skipWaiting();
 });
 
 sw.addEventListener("activate", event => {
-  event.waitUntil(sw.clients.claim());
-  event.waitUntil(sw.registration.navigationPreload.enable());
+    event.waitUntil(sw.clients.claim());
+    event.waitUntil(sw.registration.navigationPreload.enable());
 });
 
 sw.addEventListener("fetch", event => {
-  const url = new URL(event.request.url);
-  if (url.protocol.includes("extension")) return;
+    const url = new URL(event.request.url);
+    if (url.protocol.includes("extension")) return;
 
-  event.respondWith(
-    caches.open(CACHE_NAME).then(cache =>
-      fetch(event.request.url)
-        .then(res => {
-          cache.put(event.request.url, res.clone());
-          return res;
-        })
-        .catch(() => cache.match(event.request.url))
-    )
-  );
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache =>
+            fetch(event.request.url)
+                .then(res => {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                })
+                .catch(() => cache.match(event.request.url))
+        )
+    );
 });
